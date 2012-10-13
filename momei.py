6a8b5066925eed8cfe70b98eda5edb21f13d8731
Mozillazg <opensource.mozillazg@gmail.com>
2012-10-13 19:25:22 +0800

    下载单一搜索结果页包含的图片（搜索结果页面>>图片页面>>下载图片）

--- momei.py
+++ momei.py
@@ -2,14 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import re
-import logging
 
 
-def download_image(link, refere, folder):
-    pass
-
-
-def download_image(page, folder='images'):
+def get_image_link(page):
     text = page
     # png 链接所在 id
     re_png_id = re.compile(r'(?i)<a\s+[^>]*?id="png"[^>]*>')
@@ -19,9 +14,6 @@ def download_image(page, folder='images'):
     re_link = re.compile(r'(?i)href="([^"]+)"')
     png = re.findall(re_link, str(re.findall(re_png_id, text)))
     jpg = re.findall(re_link, str(re.findall(re_jpg_id, text)))
-    logger2 = logging.getLogger('download_image')
-    logger2.debug('png link: \n' + str(png))
-    logger2.debug('jpg link: \n' + str(jpg))
     if png:
         return png[0]
     elif jpg:
@@ -31,47 +23,68 @@ def download_image(page, folder='images'):
 
 
 def get_image_pages(page):
+    """analysis page content.
+    
+    return (image_pages, next_page)
+    
+    images_pages is list
+    
+    next_page is string
+    """
     text = page
-    # re_image = re.compile(r'''(?ix)<a\s[^>]*?class="thumb"[^>]*?href="([^"]+)"[^>]*?>''')
-    # <span\s[^>]*?class="plid"[^>]*?>\s*\#pl\s+((?:https?://)?(?:[^/]*)?/[^\s<]+)[^<]*?</span>
     re_image = re.compile(r'''(?ix)<span\s[^>]*?class="plid"[^>]*?>
                           \s*\#pl\s+([^\s<]+)[^<]*?</span>''')
     re_next_a = re.compile(r'(?i)<a\s[^>]*?class="next_page"[^>]*>')
     re_next = re.compile(r'(?i)href="([^"]+)"')
-    logger2 = logging.getLogger('get_image_pages')
     image_pages = re.findall(re_image, text)
-    next_pages  = re.findall(re_next, str(re.findall(re_next_a, text)))
-    logger2.debug('image pages: \n' + str(image_pages))
-    logger2.debug('next page: ' + str(next_pages))
+    next_pages = re.findall(re_next, str(re.findall(re_next_a, text)))
     next_page_ = next_pages[0] if next_pages else None
     image_pages_ = image_pages if image_pages else None
     return image_pages_, next_page_
 
-
 if __name__ == '__main__':
-    # import logging
+    import urllib2
+    import time
+    import copy
     import charset
-    logging.basicConfig(level=logging.DEBUG)
-    # tag = raw_input(u'tag: ')
-    # site = ''
-    # query = 'https://yande.re/post?tags=kyubey'
-    # 先通过request检测编码在使用 charset
-    logger1 = logging.getLogger('__main__')
-    logger1.debug('read file')
-    with open(r'temp/227202.htm') as f:
-        text1 = f.read()
-        # logger.debug('file content\n' + repr(text1))
-        text1 = charset.decode_(text1)
-        # logger.debug('unicode file content\n' + repr(text1))
-        logger1.debug('start download_image')
-        image_link = download_image(text1)
-        logger1.debug('return: ' + str(image_link))
-    logger1.debug('read file')
-    with open(r'temp/_kyubey.html') as f:
-        text2 = f.read()
-        # logger1.debug('file content\n' + repr(text2))
-        text2 = charset.decode_(text2)
-        # logger1.debug('unicode file content\n' + repr(text2))
-        logger1.debug('start get_image_pages')
-        image_link = get_image_pages(text2)
-        logger1.debug('return:\n' + str(image_link))
+    import requests
+    # 获取输入的 tag，并按 utf8 编码
+    tag = re.sub(r'\s+', '_', charset.decode_(raw_input(u'tag: ').strip())
+                 ).encode('utf8')
+    referer = site = 'https://yande.re/'
+    # 配置 requests
+    #payload = {'tags': tag}
+    url = 'https://yande.re/post?tags=%s' % (urllib2.quote(tag))
+    config = {'store_cookies' : False}
+    headers = {
+        'Host': 'yande.re',
+        'User-Agent:': ('Mozilla/5.0 (Windows NT 6.2; rv:15.0) Gecko'
+                        + '/20100101 Firefox/15.0.1'),
+        'Connection': 'keep-alive',
+        'Referer': referer
+    }
+    r = requests.get(url, headers=headers, params={},
+                     config=config, prefetch=False)
+    if r.status_code == requests.codes.ok:
+        image_pages , next_page = get_image_pages(r.content)
+        for i in image_pages:
+            if not i.startswith('http'):
+                i = site + i
+        #if not next_page.startswith('http'):
+        #    next_page += site
+        for link in image_pages:
+            r2 = requests.get(link, headers=headers,
+                              config=config, prefetch=False)
+            if r2.status_code == requests.codes.ok:
+                image_link = get_image_link(r2.content)
+                print image_link
+                headers_ = copy.deepcopy(headers)
+                headers_.update({'Referer': link})
+                r3 = requests.get(image_link, headers=headers_,
+                                  config=config, prefetch=False)
+                if r3.status_code == requests.codes.ok:
+                    file_name = urllib2.unquote(image_link.split('/')[-1])
+                    with open(file_name, 'ab', buffering=0) as f:
+                        for i in r3.iter_content(chunk_size=1*1024):
+                            f.write(i)
+                time.sleep(1*60)
